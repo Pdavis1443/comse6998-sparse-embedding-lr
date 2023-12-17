bc12684b1eee62d91fdb617c8cbbc8019d007d88
Peter Davis <Pdavis1443@gmail.com>
2023-12-17 18:58:39 -0500

    New data set

--- main.py
+++ main.py
@@ -22,67 +22,84 @@ def get_data():
     y = bos.PRICE.values
     return (X, y)
 
-def get_c41():
-    X = mmread('c-41.mtx')
-    y = mmread('c-41_b.mtx')
+def get_data(afile: str, bfile: str):
+    X = mmread(afile)
+    y = mmread(bfile)
     return (X, y)
 
 
-def full_lr(X_train, X_test, y_train, y_test):
+def full_lr(A, b):
     tick = time.time()
-    lr, istop, itn, r1norm = lsqr(X_train, y_train)[:4]
+    lr, istop, itn, r1norm = lsqr(A, b)[:4]
     tock = time.time()
     runtime = tock - tick
 
+    Ax = A @ lr
+    normAx = np.linalg.norm(Ax)
     print("Full linear regression:")
-    print("\tMean squared error (train): " + str(mean_squared_error(y_train, X_train @ lr)))
-    print("\tMean squared error (test): " + str(mean_squared_error(y_test, X_test @ lr)))
+    print("\tMean squared error: " + str(mean_squared_error(b, Ax)))
+    print("\t||Ax-b||_2(squared): " + str(r1norm) + " (%f)" % r1norm**2)
+    print("\t||Ax||_2 (squared): " + str(normAx) + " (%f)" % normAx**2)
     print("\tTime to complete (s): " + str(runtime))
     
     return lr, r1norm
 
-def sparse_embedding(X_train, X_test, y_train, y_test, k):
+def sparse_embedding(A, b, k):
     tick = time.time()
-    n = X_train.shape[0]
+    n = A.shape[0]
     tick2 = time.time()
-    S = lil_array((k, X_train.shape[0]))
-    for j in range(n):
-        i = random.randint(0, k - 1)
-        a = 1 if random.randint(0, 1) == 1 else -1
-        S[i, j] = a
+    # S = lil_array((k, A.shape[0]))
+    data = np.random.choice([1,-1], n)
+    row_indices = np.random.choice(np.arange(k), n)
+    col_indices = np.arange(n)
+    # eyes = random.choices(range(k), k=n)
+    # poles = random.choices([1, -1], k=n)
+    # for j in range(n):
+    #     S[eyes[j], j] = poles[j]
+    S = coo_array((data, (row_indices, col_indices)), shape=(k, n))
     S = S.tocsr()
-    SX = S @ X_train
-    Sy = S @ y_train
+    SA = S @ A
+    Sb = S @ b
     tock2 = time.time()
-    lr, istop, itn, r1norm = lsqr(SX, Sy)[:4]
+    lr, istop, itn, r1norm = lsqr(SA, Sb)[:4]
     tock = time.time()
     runtime = tock - tick
     time_reducing = tock2 - tick2
 
+    Ax = A @ lr
+    normAx = np.linalg.norm(Ax)
+    normAxmb = np.linalg.norm(Ax - b.reshape((b.shape[0],)))
+    normSAx = np.linalg.norm(SA @ lr)
     print("Sparse embedding reduction (k = %d)" % k)
-    print("\tMean squared error (train): " + str(mean_squared_error(y_train, X_train @ lr)))
-    print("\tMean squared error (test): " + str(mean_squared_error(y_test, X_test @ lr)))
+    print("\tMean squared error: " + str(mean_squared_error(b, Ax)))
+    print("\t||Ax-b||_2(squared): " + str(normAxmb) + " (%f)" % normAxmb**2)
+    print("\t||SAx-Sb||_2(squared): " + str(r1norm) + " (%f)" % r1norm**2)
+    print("\t||Ax||_2 (squared): " + str(normAx) + " (%f)" % normAx**2)
+    print("\t||SAx||_2 (squared): " + str(normSAx) + " (%f)" % normSAx**2)
     print("\tTime to complete (s): " + str(runtime))
     print("\tTime performing Sparse reduction (s): " + str(time_reducing))
     
-    return lr, r1norm
+    return lr, normAxmb
  
-def random_subsample(X_train, X_test, y_train, y_test, k):
-    tick = time.time()
-    tick2 = time.time()
-    SX, _, Sy, _ = train_test_split(X_train, y_train, train_size=k)
-    tock2 = time.time()
-    lr, istop, itn, r1norm = lsqr(SX, Sy)[:4]
-    tock = time.time()
-    runtime = tock - tick
-    time_reducing = tock2 - tick2
+# def random_subsample(A, b, k):
+#     tick = time.time()
+#     tick2 = time.time()
+#     SA, _, Sb, _ = train_test_split(A, b, train_size=k)
+#     tock2 = time.time()
+#     lr, istop, itn, r1norm = lsqr(SA, Sb)[:4]
+#     tock = time.time()
+#     runtime = tock - tick
+#     time_reducing = tock2 - tick2
 
-    print("Random subsampling (k = %d)" % k)
-    print("\tMean squared error (train): " + str(mean_squared_error(y_train, X_train @ lr)))
-    print("\tMean squared error (test): " + str(mean_squared_error(y_test, X_test @ lr)))
-    print("\tTime to complete (s): " + str(runtime))
-    print("\tTime randomly subsampling (s): " + str(time_reducing))
-    return lr, r1norm
+#     Ax = A @ lr
+#     normAxmb = np.linalg.norm(Ax - b.reshape((b.shape[0],)))
+#     print("Random subsampling (k = %d)" % k)
+#     print("\tMean squared error: " + str(mean_squared_error(b, Ax)))
+#     print("\t||Ax-b||_2(squared): " + str(normAxmb) + " (%f)" % normAxmb**2)
+#     print("\t||SAx-Sb||_2(squared): " + str(r1norm) + " (%f)" % r1norm**2)
+#     print("\tTime to complete (s): " + str(runtime))
+#     print("\tTime randomly subsampling (s): " + str(time_reducing))
+#     return lr, normAxmb
  
  
  # Adds gaussian noise to some entries of X, ensuring that it will remain sparse if X is sparse.
@@ -109,39 +126,31 @@ def replicate(X, y, reps, seed):
     XBig = vstack(Xs)
     return (XBig, yBig)
 
-def run(X, y, reps, k_sparse, k_random):
-    X, y = replicate(X, y, reps, 1337)
-    print("Running comparison for %d reps, %d sparse embedding rows, and %d random samples" % (reps, k_sparse, k_random) )
-    print("X Shape (post reps): " + str(X.shape) + ", nnz: " + str(X.nnz))
-    print("y Shape (post reps): " + str(y.shape))
+def run(A, b, reps, k_sparse):
+    A, b = replicate(A, b, reps, 1337)
+    print("Running comparison for %d reps and %d sparse embedding rows" % (reps, k_sparse) )
+    print("A Shape (post reps): " + str(A.shape) + ", nnz: " + str(A.nnz))
+    print("b Shape (post reps): " + str(b.shape))
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-    print("Data shapes (A_train, A_test, b_train, b_test): ", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-    lr, train_err = full_lr(X_train, X_test, y_train, y_test)
-    print("||Ax-b||_2: " + str(train_err))
-    #djl(X_train, X_test, y_train, y_test, 300)
-    lr_sparse, train_err_sparse = sparse_embedding(X_train, X_test, y_train, y_test, k_sparse)
-    print("||SAx - Sb||_2 for sparse embedding: " + str(train_err_sparse))
-    lr_random, train_err_random = random_subsample(X_train, X_test, y_train, y_test, k_random)
-    print("||SAx - Sb||_2 for random subsample: " + str(train_err_random))
+    lr, train_err = full_lr(A, b)
+    #djl(A, A, b, b, 300)
+    lr_sparse, train_err_sparse = sparse_embedding(A, b, k_sparse)
 
 
 
-X, y = get_c41()
-reps = 5, 50, 500
-k_sparses = [4000, 40_000, 400_000]
-k_randoms = [4_000, 40_000, 400_000]
-for (rep, k_sparse, k_random) in zip(reps, k_sparses, k_randoms):
-    run(X, y, rep, k_sparse, k_random)
+A, b = get_data('illc1033.mtx', 'illc1033_b.mtx')
+reps = [400, 4_000, 40_000, 80_000, 200_000]
+k_sparse = 4000#40_000_000
+for rep in reps:
+    run(A, b, rep, k_sparse)
 
-# def djl(X_train, X_test, y_train, y_test, k):
+# def djl(A, A, b, b, k):
 #     tick = time.time()
-#     n = X_train.shape[0]
+#     n = A.shape[0]
 #     tick2 = time.time()
 #     S = csr_array(np.random.normal(size = (k, n)) * (1.0 / math.sqrt(k)))
-#     SX = S @ X_train
-#     Sy = S @ y_train
+#     SX = S @ A
+#     Sy = S @ b
 #     tock2 = time.time()
 #     lr, istop, itn, r1norm = lsqr(SX, Sy)[:4]
 #     tock = time.time()
@@ -149,8 +158,8 @@ for (rep, k_sparse, k_random) in zip(reps, k_sparses, k_randoms):
 #     time_reducing = tock2 - tick2
 
 #     print("DJL reduction (k = %d)" % k)
-#     print("\tMean absolute error: " + str(mean_absolute_error(y_test, X_test @ lr)))
-#     print("\tMean squared error: " + str(mean_squared_error(y_test, X_test @ lr)))
+#     print("\tMean absolute error: " + str(mean_absolute_error(b, A @ lr)))
+#     print("\tMean squared error: " + str(mean_squared_error(b, A @ lr)))
 #     print("\tTime to complete (s): " + str(runtime))
 #     print("\tTime performing DJL reduction (s): " + str(time_reducing))
 #     print("\tIterations: " + str(itn))
